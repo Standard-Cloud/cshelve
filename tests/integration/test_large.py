1659ec819aca44d8c5670a417c7e93e39809d1a6
florian-vuillemot <vuillemot.florian@outlook.fr>
2024-10-12 15:56:29 +0200

    init project

---
+++ tests/integration/test_large.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from cshelve import open
+
+
+def test_large():
+    db = open('tests/configurations/integration-azure.ini')
+
+    key_pattern = 'test_large'
+
+    # 167.46 MiB
+    df = pd.DataFrame(np.random.randint(0, 100, size=(844221, 26)), columns=list('ABCDEFGHIGKLMNOPQRSTUVWXYZ'))
+
+    db[key_pattern] = df
+    new_df = db[key_pattern]
+
+    assert id(new_df) != id(df)
+    assert new_df.equals(df)
