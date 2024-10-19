8db7a0c903af24880039fbeeaeace470530ab751
Vuillemot Florian <vuillemot.florian@outlook.fr>
2024-10-19 12:56:15 +0200

    Add test on the protocol arg (#15)

---
+++ tests/integration/test_protocol.py
@@ -0,0 +1,39 @@
+import pickle
+
+import cshelve
+
+
+def test_protocol():
+    """
+    Ensure cshelve works correctly with the non default protocol.
+    """
+    config_file = "tests/configurations/azure-integration/standard.ini"
+    key_pattern = "test_protocol"
+    data_pattern = "test_protocol"
+    protocol = pickle.HIGHEST_PROTOCOL
+
+    with cshelve.open(config_file, protocol=protocol) as db:
+        for i in range(100):
+            db[f"{key_pattern}{i}"] = f"{data_pattern}{i}"
+
+    with cshelve.open(config_file, protocol=protocol) as db:
+        for i in range(100):
+            assert f"{data_pattern}{i}" == db[f"{key_pattern}{i}"]
+
+
+def test_change_protocol():
+    """
+    Ensure cshelve works correctly with the non default protocol.
+    """
+    config_file = "tests/configurations/azure-integration/standard.ini"
+    key_pattern = "test_protocol"
+    data_pattern = "test_protocol"
+    protocol = pickle.HIGHEST_PROTOCOL
+
+    with cshelve.open(config_file, protocol=protocol) as db:
+        for i in range(100):
+            db[f"{key_pattern}{i}"] = f"{data_pattern}{i}"
+
+    with cshelve.open(config_file, protocol=protocol) as db:
+        for i in range(100):
+            assert f"{data_pattern}{i}" == db[f"{key_pattern}{i}"]
