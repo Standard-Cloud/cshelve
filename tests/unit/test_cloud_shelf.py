102554154cd4bc672cc60aecea9ad0c5bbd1966d
florian-vuillemot <vuillemot.florian@outlook.fr>
2024-11-01 12:19:15 +0100

    Refactor tests

--- tests/unit/test_cloud_shelf.py
+++ tests/unit/test_cloud_shelf.py
@@ -11,7 +11,7 @@ def test_factory_usage():
     """
     Ensure the usage of the factory.
     """
-    filename = "./tests/configurations/azure.ini"
+    filename = "does_not_exists.ini"
     provider = "azure-blob"
     config = {42: 42}
     flag = "c"
@@ -34,11 +34,36 @@ def test_factory_usage():
         assert isinstance(cs.dict.db, Mock)
 
 
+def test_loader_usage():
+    """
+    Ensure the usage of the loader.
+    """
+    filename = "does_not_exists.ini"
+    provider = "azure-blob"
+    config = {42: 42}
+    flag = "c"
+    protocol = pickle.HIGHEST_PROTOCOL
+    writeback = False
+
+    factory = Mock()
+    loader = Mock()
+    cloud_database = Mock()
+
+    loader.return_value = provider, config
+    factory.return_value = cloud_database
+    cloud_database.exists.return_value = False
+
+    with CloudShelf(
+        filename, flag, protocol, writeback, loader=loader, factory=factory
+    ) as cs:
+        loader.assert_called_once_with(filename)
+
+
 def test_provider_configuration():
     """
     Ensure the provider returns by the factory is configured.
     """
-    filename = "./tests/configurations/azure.ini"
+    filename = "does_not_exists.ini"
     provider = "azure-blob"
     config = {42: 42}
     flag = "c"
--- tests/unit/test_parser.py
+++ tests/unit/test_parser.py
@@ -2,53 +2,7 @@
 Depending on the filename, either the native shelve module or the cloud shelve module is used.
 The cloud shelve module is used when the filename has a specific extension, and we must ensure that the correct module is used.
 """
-import shelve
-import tempfile
-from unittest.mock import Mock
-
-import cshelve
 from cshelve._parser import load, use_local_shelf
-
-
-def test_load_cloud_shelf_config():
-    """
-    Based on the filename, the cloud shelve module must be used.
-    At the same time, we test the parser injection functionality.
-    """
-    filename = "test.ini"
-    provider = "myprovider"
-    config = {
-        "provider": provider,
-        "auth_type": "passwordless",
-        "container_name": "mycontainer",
-    }
-
-    cdit = Mock()
-    factory = Mock()
-    loader = Mock()
-
-    factory.return_value = cdit
-    loader.return_value = provider, config
-
-    # Replace the default parser with the mock parser.
-    cshelve.open(filename, loader=loader, factory=factory)
-
-    loader.assert_called_once_with(filename)
-
-
-def test_load_local_shelf_config():
-    """
-    Based on the filename, the default shelve module must be used.
-    """
-    local_shelf_suffix = ["sqlite3", "db", "dat"]
-
-    for suffix in local_shelf_suffix:
-        # When instanciate, shelf modules create the file with the provided name.
-        # So we create a temporary file to garbage collect it after the test.
-        with tempfile.NamedTemporaryFile(suffix=suffix) as fp:
-            fp.close()
-            default = cshelve.open(fp.name)
-            assert isinstance(default, shelve.DbfilenameShelf)
 
 
 def test_use_local_shelf():
