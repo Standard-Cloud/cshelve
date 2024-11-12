601ef3e110bf93d78840c2fd68c13ca626b6f2f2
Vuillemot Florian <florian.vuillemot@avaiga.com>
2024-11-12 19:02:51 +0000

    prepare version 0.4.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,7 +9,7 @@
 project = "cshelve"
 copyright = "2024, Vuillemot Florian"
 author = "Vuillemot Florian"
-release = "0.3.0"
+release = "0.4.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
