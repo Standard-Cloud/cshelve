dfe994bb8c5720a19ce9c695fd5678b260637961
Vuillemot Florian <vuillemot.florian@outlook.fr>
2024-11-26 07:59:27 +0100

    Release/0.6.0 (#69)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,7 +9,7 @@
 project = "cshelve"
 copyright = "2024, Vuillemot Florian"
 author = "Vuillemot Florian"
-release = "0.5.0"
+release = "0.6.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
