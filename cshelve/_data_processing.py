1f03f2b0febf444dfb8af41cf57d05459049bcc0
florian-vuillemot <vuillemot.florian@outlook.fr>
2025-01-22 07:56:27 +0000

    add signature in data processing

--- cshelve/_data_processing.py
+++ cshelve/_data_processing.py
@@ -3,8 +3,8 @@ This module provides the DataProcessing class, which handles pre-processing and 
 
 Examples:
     >>> dp = DataProcessing()
-    >>> dp.add_pre_processing(lambda x: x + b'1')
-    >>> dp.add_post_processing(lambda x: x + b'2')
+    >>> dp.add_pre_processing('add_1', lambda x: x + b'1')
+    >>> dp.add_post_processing('add_2', lambda x: x + b'2')
     >>> pre_processed = dp.apply_pre_processing(b'0')
     >>> pre_processed
     b'01'
@@ -12,21 +12,31 @@ Examples:
     >>> post_processed
     b'012'
 """
-from typing import Callable, Optional, List
+from collections import namedtuple
+from typing import Callable, List
 
+
+_Process = namedtuple(
+    "_Process",
+    ["name", "function"],
+)
+
+
+# Algorithm signatures to applied to the data.
+# Used with a XOR to ensure that the data is processed correctly.
+SIGNATURES = {
+    'COMPRESSION': 0b00000001,
+    'ENCRYPTION':  0b00000010
+}
 
 class DataProcessing:
     """
     A class to handle pre-processing and post-processing of data.
     """
 
-    def __init__(
-        self,
-        pre_processing: Optional[List[Callable[[bytes], bytes]]] = None,
-        post_processing: Optional[List[Callable[[bytes], bytes]]] = None,
-    ):
+    def __init__(self):
         """
-        Initializes the DataProcessing class with optional pre-processing and post-processing lists.
+        Initializes the DataProcessing class.
 
         Examples:
         >>> dp = DataProcessing()
@@ -35,22 +45,24 @@ class DataProcessing:
         >>> dp.post_processing
         []
         """
-        self.pre_processing = pre_processing if pre_processing is not None else []
-        self.post_processing = post_processing if post_processing is not None else []
+        self.pre_processing: List[_Process] = []
+        self.post_processing: List[_Process] = []
 
-    def add_pre_processing(self, func: Callable[[bytes], bytes]) -> None:
+    def add_pre_processing(self, name: str, func: Callable[[bytes], bytes]) -> None:
         """
         Adds a function to the pre-processing list.
 
         Examples:
         >>> dp = DataProcessing()
-        >>> dp.add_pre_processing(lambda x: x + 1)
+        >>> dp.add_pre_processing('add_1', lambda x: x + 1)
         >>> len(dp.pre_processing)
         1
         """
-        self.pre_processing.append(func)
+        self.pre_processing.append(
+            _Process(name=name, function=func)
+        )
 
-    def add_post_processing(self, func: Callable[[bytes], bytes]) -> None:
+    def add_post_processing(self, name: str, func: Callable[[bytes], bytes]) -> None:
         """
         Adds a function to the post-processing list.
 
@@ -59,11 +71,13 @@ class DataProcessing:
 
         Examples:
         >>> dp = DataProcessing()
-        >>> dp.add_post_processing(lambda x: x * 2)
+        >>> dp.add_post_processing('mult_by_2', lambda x: x * 2)
         >>> len(dp.post_processing)
         1
         """
-        self.post_processing.append(func)
+        self.post_processing.append(
+            _Process(name=name, function=func)
+        )
 
     def apply_pre_processing(self, data: bytes) -> bytes:
         """
@@ -76,12 +90,14 @@ class DataProcessing:
         The processed data.
 
         Examples:
-        >>> dp = DataProcessing(pre_processing=[lambda x: x + 1, lambda x: x * 2])
+        >>> dp = DataProcessing()
+        >>> dp.add_pre_processing('add_1', lambda x: x + 1)
+        >>> dp.add_pre_processing('mult_by_2', lambda x: x * 2)
         >>> dp.apply_pre_processing(1)
         4
         """
-        for func in self.pre_processing:
-            data = func(data)
+        for p in self.pre_processing:
+            data = p.function(data)
         return data
 
     def apply_post_processing(self, data: bytes) -> bytes:
@@ -95,10 +111,15 @@ class DataProcessing:
         The processed data.
 
         Examples:
-        >>> dp = DataProcessing(post_processing=[lambda x: x / 2, lambda x: x - 1])
+        >>> dp = DataProcessing()
+        >>> dp.add_post_processing('div_by_2', lambda x: x / 2)
+        >>> dp.add_post_processing('minus_1', lambda x: x - 1)
         >>> dp.apply_post_processing(4)
         1.0
         """
-        for func in self.post_processing:
-            data = func(data)
+        for p in self.post_processing:
+            data = p.function(data)
         return data
+
+    def pre_processing_signature(self):
+        return [p.name for p in self.pre_processing]
--- tests/units/test_data_processing.py
+++ tests/units/test_data_processing.py
@@ -2,36 +2,45 @@ from cshelve import DataProcessing
 
 
 def test_add_pre_processing():
+    name = 'name_test_add_pre_processing'
     dp = DataProcessing()
-    dp.add_pre_processing(lambda x: x + 1)
+    dp.add_pre_processing(name, lambda x: x + 1)
     assert len(dp.pre_processing) == 1
-    assert dp.pre_processing[0](1) == 2
+    assert dp.pre_processing[0].name == name
+    assert dp.pre_processing[0].function(1) == 2
 
 
 def test_add_post_processing():
+    name = 'name_test_add_post_processing'
     dp = DataProcessing()
-    dp.add_post_processing(lambda x: x * 2)
+    dp.add_post_processing(name, lambda x: x * 2)
     assert len(dp.post_processing) == 1
-    assert dp.post_processing[0](2) == 4
+    assert dp.post_processing[0].name == name
+    assert dp.post_processing[0].function(2) == 4
 
 
 def test_apply_pre_processing():
-    dp = DataProcessing(pre_processing=[lambda x: x + 1, lambda x: x * 2])
+    dp = DataProcessing()
+    dp.add_pre_processing('add_1', lambda x: x + 1)
+    dp.add_pre_processing('mult_by_2', lambda x: x * 2)
     result = dp.apply_pre_processing(1)
     assert result == 4  # (1 + 1) * 2
 
 
 def test_apply_post_processing():
-    dp = DataProcessing(post_processing=[lambda x: x / 2, lambda x: x - 1])
+    dp = DataProcessing()
+    dp.add_post_processing('div_by_2', lambda x: x / 2)
+    dp.add_post_processing('minus_1', lambda x: x - 1)
     result = dp.apply_post_processing(4)
     assert result == 1  # (4 / 2) - 1
 
 
 def test_full_processing():
-    dp = DataProcessing(
-        pre_processing=[lambda x: x + 1, lambda x: x * 2],
-        post_processing=[lambda x: x / 2, lambda x: x - 1],
-    )
+    dp = DataProcessing()
+    dp.add_pre_processing('add_1', lambda x: x + 1)
+    dp.add_pre_processing('mult_by_2', lambda x: x * 2)
+    dp.add_post_processing('div_by_2', lambda x: x / 2)
+    dp.add_post_processing('minus_1', lambda x: x - 1)
 
     data = 1
     pre_processed_data = dp.apply_pre_processing(data)
