8323a5787727f6c74b8990d1cd1a568cf315f0ca
florian-vuillemot <vuillemot.florian@outlook.fr>
2025-01-27 18:39:48 +0000

    do not run very large test

--- tests/end-to-end/test_large.py
+++ tests/end-to-end/test_large.py
@@ -46,27 +46,28 @@ def test_large(config_file):
     assert new_df.equals(df)
 
 
-@pytest.mark.azure
-@pytest.mark.parametrize(
-    "config_file",
-    CONFIG_FILES,
-)
-def test_very_large(config_file):
-    """
-    Update a relative large DataFrame in the DB to verify it is possible.
-    """
-    db = cshelve.open(config_file, protocol=4)
+# This test is disabled because Standard GitHub Runner for Open Source project can't run it.
+# @pytest.mark.azure
+# @pytest.mark.parametrize(
+#     "config_file",
+#     CONFIG_FILES,
+# )
+# def test_very_large(config_file):
+#     """
+#     Update a relative large DataFrame in the DB to verify it is possible.
+#     """
+#     db = cshelve.open(config_file, protocol=4)
 
-    key_pattern = unique_key + "test_very_large"
+#     key_pattern = unique_key + "test_very_large"
 
-    # 5.62 GiB
-    df = pd.DataFrame(
-        np.random.randint(0, 100, size=(13507536, 52)),
-        columns=list("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"),
-    )
+#     # 5.62 GiB
+#     df = pd.DataFrame(
+#         np.random.randint(0, 100, size=(13507536, 52)),
+#         columns=list("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+#     )
 
-    db[key_pattern] = df
-    new_df = db[key_pattern]
+#     db[key_pattern] = df
+#     new_df = db[key_pattern]
 
-    assert id(new_df) != id(df)
-    assert new_df.equals(df)
+#     assert id(new_df) != id(df)
+#     assert new_df.equals(df)
