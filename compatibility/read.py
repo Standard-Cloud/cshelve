650d8571aa9beeaa06d6517d5c8192a658ec5b3e
florian-vuillemot <vuillemot.florian@outlook.fr>
2025-01-30 23:13:21 +0000

    fix assertion

--- compatibility/read.py
+++ compatibility/read.py
@@ -8,6 +8,6 @@ python_version = sys.argv[2]
 
 with cshelve.open("./azure-passwordless.ini") as db:
     assert (
-        f"compatibility-{cshelve_version}-{python_version}"
+        db[f"compatibility-{cshelve_version}-{python_version}"]
         == f"my complex data from cshelve version {cshelve_version} and python {python_version}"
     )
