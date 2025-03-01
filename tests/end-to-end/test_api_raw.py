897ab82910c464ac3172cc3fc62441c62ebe406b
florian-vuillemot <vuillemot.florian@outlook.fr>
2025-03-01 09:42:55 +0000

    add test

---
+++ tests/end-to-end/test_api_raw.py
@@ -0,0 +1,45 @@
+"""
+Ensure the standard behavior of the API works as expected in real scenarios.
+"""
+import pytest
+
+import cshelve
+
+from helpers import write_data, unique_key, del_data
+
+
+CONFIG_FILES_RAW = [
+    "tests/configurations/aws-s3/raw.ini",
+    "tests/configurations/azure-blob/raw.ini",
+    "tests/configurations/in-memory/raw.ini",
+]
+
+
+@pytest.mark.parametrize(
+    "config_file",
+    CONFIG_FILES_RAW,
+)
+def test_raw(config_file: str):
+    """
+    Ensure the mode 'raw' upload file without any processing.
+    """
+    with cshelve.open(config_file) as db:
+        key_pattern = f"{unique_key}-test_raw-{config_file}"
+        data_pattern = "test_raw"
+
+        for i in range(10):
+            key = f"{key_pattern}{i}"
+            data = {
+                "my_value": f"{data_pattern}{i}",
+            }
+
+            # Write data to the DB.
+            db[key] = data
+            # Data must be present in the DB.
+            assert db[key] == data
+            # Ensure data doesn't contains any processing.
+            assert db.dict.db.get(key.encode()) == data
+            # Delete the data from the DB.
+            del db[key]
+
+    db.close()
