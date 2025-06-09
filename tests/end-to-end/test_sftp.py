17b93879ee52581b280b617cf513def06ba8b5a0
Vuillemot Florian <vuillemot.florian@outlook.fr>
2025-06-09 13:16:10 +0000

    skip ssh key auth

--- tests/end-to-end/test_sftp.py
+++ tests/end-to-end/test_sftp.py
@@ -33,6 +33,7 @@ def test_sftp_authentication(config_file):
         "tests/configurations/sftp/auth_ssh_ed25519.ini",
     ],
 )
+@pytest.mark.skip(reason="No SSH key on the CI server.")
 def test_sftp_auth_methods(config_file):
     """
     Test SFTP auth methods.
