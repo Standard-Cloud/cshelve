ecccd800ad9c143e9bafeda72af61be27a2e57b3
Florian Vuillemot <vuillemot.florian@outlook.fr>
2025-06-18 16:50:03 +0000

    update iter result

--- cshelve/_sftp.py
+++ cshelve/_sftp.py
@@ -377,4 +377,4 @@ class SFTP(ProviderInterface):
                 yield from self._iter(full_path)
             else:
                 self.logger.debug(f"Yielding key: {item}")
-                yield item.encode("utf-8")
+                yield full_path
