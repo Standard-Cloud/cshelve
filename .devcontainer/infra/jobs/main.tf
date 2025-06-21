2033e702787b33ecc4720e32ba23168520fa6484
Florian Vuillemot <vuillemot.florian@outlook.fr>
2025-06-21 07:04:25 +0000

    debug

--- .devcontainer/infra/jobs/main.tf
+++ .devcontainer/infra/jobs/main.tf
@@ -17,10 +17,10 @@ data "azurerm_storage_account" "storage" {
 locals {
   # Clean up os and python_version for container name
   clean_os = replace(lower(var.os), "[^a-z0-9]", "")
-  clean_python_version = replace(replace(var.python_version, ".", "-"), "[^a-z0-9_]", "")
+  clean_python_version = replace(replace(var.python_version, ".", ""), "[^a-z0-9_]", "")
 
   # Create a unique container name for this job
-  container_name = lower("sftp-job-${local.clean_os}-python${local.clean_python_version}")
+  container_name = lower("sftpjob${local.clean_os}python${local.clean_python_version}")
 }
 
 # Create a container for this specific job
