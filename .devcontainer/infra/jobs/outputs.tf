00c101c5f03a712748dbced3328f406020666ab3
Florian Vuillemot <vuillemot.florian@outlook.fr>
2025-06-21 09:48:15 +0000

    update secret passing

--- .devcontainer/infra/jobs/outputs.tf
+++ .devcontainer/infra/jobs/outputs.tf
@@ -6,8 +6,3 @@ output "storage_account_name" {
 output "sftp_name" {
   value = azurerm_storage_container.job_sftp.name
 }
-
-output "storage_account_primary_connection_string" {
-  value     = data.azurerm_storage_account.storage.primary_connection_string
-  sensitive = true
-}
