01c61c39a54732f2cc755a7beee5e7ab7ade5a8f
Florian Vuillemot <vuillemot.florian@outlook.fr>
2025-06-21 06:22:37 +0000

    replace . in container name

--- .devcontainer/infra/backbone/main.tf
+++ .devcontainer/infra/backbone/main.tf
@@ -43,3 +43,45 @@ resource "azurerm_storage_account" "storage" {
     purpose       = "cshelve-sftp-testing"
   }
 }
+
+# Generate an SSH key for SFTP access
+resource "tls_private_key" "sftp_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create a random password for SFTP user
+resource "random_password" "sftp_password" {
+  length           = 16
+  special          = true
+  override_special = "!@#$%&*()-_=+[]{}<>:?"
+}
+
+# Create local user for SFTP access
+resource "azurerm_storage_account_local_user" "sftp_user" {
+  name                 = "cshelveuser"
+  storage_account_id   = azurerm_storage_account.storage.id
+  home_directory       = "/"
+  ssh_password_enabled = true
+  ssh_key_enabled      = true
+
+  permission_scope {
+    permissions {
+      read   = true
+      write  = true
+      delete = true
+      list   = true
+      create = true
+    }
+    service       = "blob"
+    resource_name = "*"
+  }
+
+  ssh_authorized_key {
+    description = "SFTP access key"
+    key         = tls_private_key.sftp_ssh_key.public_key_openssh
+  }
+
+  # Set password for SFTP user
+  password = random_password.sftp_password.result
+}
