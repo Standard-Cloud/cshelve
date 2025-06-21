a15ed6ade1b5a617115133655a36ceb532294732
Florian Vuillemot <vuillemot.florian@outlook.fr>
2025-06-21 06:18:51 +0000

    replace . in container name

--- .devcontainer/infra/jobs/main.tf
+++ .devcontainer/infra/jobs/main.tf
@@ -17,7 +17,7 @@ data "azurerm_storage_account" "storage" {
 locals {
   # Clean up os and python_version for container name
   clean_os = replace(lower(var.os), "[^a-z0-9]", "")
-  clean_python_version = replace(var.python_version, "[^a-z0-9]", "")
+  clean_python_version = replace(replace(var.python_version, ".", "_"), "[^a-z0-9_]", "")
 
   # Create a unique container name for this job
   container_name = lower("sftp-job-${local.clean_os}-python${local.clean_python_version}")
