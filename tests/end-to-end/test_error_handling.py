e22e9e8f7a4f4acf47dd3f2979bec6a009a6f1af
Vuillemot Florian <vuillemot.florian@outlook.fr>
2025-12-21 17:04:32 +0000

    update tests

--- tests/end-to-end/test_error_handling.py
+++ tests/end-to-end/test_error_handling.py
@@ -8,7 +8,7 @@ import cshelve
 
 CONFIG_FILES_KEY_NOT_FOUND = [
     "tests/configurations/azure-blob/standard.ini",
-    "tests/configurations/filesystem/persisted.ini",
+    "tests/configurations/filesystem/standard.ini",
     "tests/configurations/in-memory/persisted.ini",
     "tests/configurations/aws-s3/standard.ini",
 ]
